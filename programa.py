2f563cf31c636cc8e004186cdb139a6ac0e58f47
Eliézer Luz Ribeiro <eliezerlribeiro@gmail.com>
2017-02-07 15:23:19 -0200

    Update
    Alterado para funcionar o parser e o resolvedor sat

--- programa.py
+++ programa.py
@@ -1,3 +1,7 @@
+#coding: utf-8
+
+import sys
+import netlist
 import pycosat
 class Circuit:
     def __init__(self):
@@ -7,9 +11,9 @@ class Circuit:
     def add_gate(self,gate):
         self.gates.append(gate)
     def add_input(self,inp):
-        self.inputs.append(inp)
+        self.inputs=self.inputs+inp
     def add_output(self,output):
-        self.outputs.append(output)
+        self.outputs=self.outputs+output
     def refactor_num(self,num, is_odd):
         foo = num*2
         if is_odd:
@@ -51,133 +55,136 @@ class Gate:
         self.gate=gate
         self.inputs = inputs
         self.output = output
-c1 = Circuit()
-c1.add_input(6)
-c1.add_input(7)
-c1.add_input(8)
-c1.add_gate(Gate("nand",[6,7],1))
-c1.add_gate(Gate("nor",[7,8],2))
-c1.add_gate(Gate("not",[1],3))
-c1.add_gate(Gate("or",[1,2],4))
-c1.add_gate(Gate("and",[3,4],5))
-c1.add_output(5)
-
-c2 = Circuit()
-c2.add_input(1)
-c2.add_input(4)
-c2.add_input(5)
-c2.add_gate(Gate("not",[1],2))
-c2.add_gate(Gate("and",[1,2],3))
-
-c2.add_output(3)
 
 
+def main(argv):
+	arqC1 = open(argv[0], 'r')
+	c3 = netlist.Netlist(arqC1)
+	
+	arqC2 = open(argv[1], 'r')
+	c4 = netlist.Netlist(arqC2)
 
-#gera ids únicos para evitar repetição
-c1.refactor(True)
-c2.refactor(False)
+	c1 = Circuit()
+	c1.add_input(c4.inputs)	
+	c1.add_output(c4.outputs)	
+	
+	for gate in c3.gates:
+		c1.add_gate(Gate(gate[0],gate[1],gate[2]))
 
+	c2 = Circuit()
+	c2.add_input(c3.inputs)
+	c2.add_output(c3.outputs)
+	for gate in c4.gates:
+		c2.add_gate(Gate(gate[0],gate[1],gate[2]))
+	
 
-#se número de saídas for diferente, encerra programa
-if len(c1.inputs) != len(c2.inputs):
-    print("Circuitos com número incompatível de entradas")
-    quit()
-    
-#se número de saídas for diferente, encerra programa
-if len(c1.outputs) != len(c2.outputs):
-    print("Circuitos com número incompatível de saídas")
-    quit()
-
-
-#empurra os ids dos circuitos para dar espaço às portas adicionais
-additional = len(c1.outputs)
-if additional>1:
-    additional+=1
-c1.push_numbers(additional)
-c2.push_numbers(additional)
-
-
-#refatora um circuito para receber as mesmas entradas do primeiro
-c2.remap_inputs(c1.inputs)
+	#gera ids únicos para evitar repetição
+	c1.refactor(True)
+	c2.refactor(False)
 
 
-final_gates=[]#portas a serem verificadas no SAT
-final_gate_inputs=[]#entradas para o OR da última porta
-#adiciona XOR's
-for i in range(len(c1.outputs)):
-    final_gates.append(Gate("xor",[ c1.outputs[i], c2.outputs[i] ], i+1 ))
-    final_gate_inputs.append(i+1)
-if len(c1.outputs)>1:
-    final_gates.append(Gate("or",final_gate_inputs,additional))
-
-for gate in c1.gates:
-    final_gates.append(gate)
-
-    
-for gate in c2.gates:
-    final_gates.append(gate)
-
-#Gerar CNF
-
-phi=[[additional]]
+	#se número de saídas for diferente, encerra programa
+	if len(c1.inputs) != len(c2.inputs):
+		print("Circuitos com número incompatível de entradas")
+		quit()
+		
+	#se número de saídas for diferente, encerra programa
+	if len(c1.outputs) != len(c2.outputs):
+		print("Circuitos com número incompatível de saídas")
+		quit()
 
 
-for gate in final_gates:
-    
-    if gate.gate=="or":
-        for i in range(len(gate.inputs)):
-            phi.append([-gate.inputs[i] , gate.output])
-        last = []
-        for i in range(len(gate.inputs)):
-            last.append(gate.inputs[i])
-        last.append(-gate.output)
-        phi.append(last)
-        
-    elif gate.gate=="not":
-        phi.append([gate.inputs[0] , gate.output])
-        phi.append([-gate.inputs[0] , -gate.output])
-                   
-    elif gate.gate=="and":
-        for i in range(len(gate.inputs)):
-            phi.append([gate.inputs[i] , -gate.output])
-        last = []
-        for i in range(len(gate.inputs)):
-            last.append(-gate.inputs[i])
-        last.append(gate.output)
-        phi.append(last)
+	#empurra os ids dos circuitos para dar espaço às portas adicionais
+	additional = len(c1.outputs)
+	if additional>1:
+		additional+=1
+	c1.push_numbers(additional)
+	c2.push_numbers(additional)
 
-    elif gate.gate=="nor":
-        for i in range(len(gate.inputs)):
-            phi.append([-gate.inputs[i] , -gate.output])
-        last = []
-        for i in range(len(gate.inputs)):
-            last.append(gate.inputs[i])
-        last.append(gate.output)
-        phi.append(last)
 
-                    
-    elif gate.gate=="nand":
-        for i in range(len(gate.inputs)):
-            phi.append([gate.inputs[i] , gate.output])
-        last = []
-        for i in range(len(gate.inputs)):
-            last.append(-gate.inputs[i])
-        last.append(-gate.output)
-        phi.append(last)
+	#refatora um circuito para receber as mesmas entradas do primeiro
+	c2.remap_inputs(c1.inputs)
 
-    elif gate.gate=="xor":
-        phi.append([-gate.inputs[0] , -gate.inputs[1] ,-gate.output])
-        phi.append([gate.inputs[0] , gate.inputs[1] ,-gate.output])
-        phi.append([gate.inputs[0] , -gate.inputs[1] ,gate.output])
-        phi.append([-gate.inputs[0] , gate.inputs[1] ,gate.output])
 
-    elif gate.gate=="xnor":
-        phi.append([-gate.inputs[0] , -gate.inputs[1] ,gate.output])
-        phi.append([gate.inputs[0] , gate.inputs[1] ,gate.output])
-        phi.append([gate.inputs[0] , -gate.inputs[1] ,-gate.output])
-        phi.append([-gate.inputs[0] , gate.inputs[1] ,-gate.output])
+	final_gates=[]#portas a serem verificadas no SAT
+	final_gate_inputs=[]#entradas para o OR da última porta
+	#adiciona XOR's
+	for i in range(len(c1.outputs)):
+		final_gates.append(Gate("xor",[ c1.outputs[i], c2.outputs[i] ], i+1 ))
+		final_gate_inputs.append(i+1)
+	if len(c1.outputs)>1:
+		final_gates.append(Gate("or",final_gate_inputs,additional))
 
-#for gate in final_gates:
-    #print("gate",gate.inputs,gate.output);
-#RESOLVER SAT
-print(pycosat.solve(phi))
+	for gate in c1.gates:
+		final_gates.append(gate)
+
+		
+	for gate in c2.gates:
+		final_gates.append(gate)
+
+	#Gerar CNF
+
+	phi=[[additional]]
+
+
+	for gate in final_gates:
+		if gate.gate=="or":
+		    for i in range(len(gate.inputs)):
+		        phi.append([-gate.inputs[i] , gate.output])
+		    last = []
+		    for i in range(len(gate.inputs)):
+		        last.append(gate.inputs[i])
+		    last.append(-gate.output)
+		    phi.append(last)
+		    
+		elif gate.gate=="not":
+		    phi.append([gate.inputs[0] , gate.output])
+		    phi.append([-gate.inputs[0] , -gate.output])
+		               
+		elif gate.gate=="and":
+		    for i in range(len(gate.inputs)):
+		        phi.append([gate.inputs[i] , -gate.output])
+		    last = []
+		    for i in range(len(gate.inputs)):
+		        last.append(-gate.inputs[i])
+		    last.append(gate.output)
+		    phi.append(last)
+
+		elif gate.gate=="nor":
+		    for i in range(len(gate.inputs)):
+		        phi.append([-gate.inputs[i] , -gate.output])
+		    last = []
+		    for i in range(len(gate.inputs)):
+		        last.append(gate.inputs[i])
+		    last.append(gate.output)
+		    phi.append(last)
+
+		                
+		elif gate.gate=="nand":
+		    for i in range(len(gate.inputs)):
+		        phi.append([gate.inputs[i] , gate.output])
+		    last = []
+		    for i in range(len(gate.inputs)):
+		        last.append(-gate.inputs[i])
+		    last.append(-gate.output)
+		    phi.append(last)
+
+		elif gate.gate=="xor":
+		    phi.append([-gate.inputs[0] , -gate.inputs[1] ,-gate.output])
+		    phi.append([gate.inputs[0] , gate.inputs[1] ,-gate.output])
+		    phi.append([gate.inputs[0] , -gate.inputs[1] ,gate.output])
+		    phi.append([-gate.inputs[0] , gate.inputs[1] ,gate.output])
+
+		elif gate.gate=="xnor":
+		    phi.append([-gate.inputs[0] , -gate.inputs[1] ,gate.output])
+		    phi.append([gate.inputs[0] , gate.inputs[1] ,gate.output])
+		    phi.append([gate.inputs[0] , -gate.inputs[1] ,-gate.output])
+		    phi.append([-gate.inputs[0] , gate.inputs[1] ,-gate.output])
+
+	#for gate in final_gates:
+		#print("gate",gate.inputs,gate.output);
+	#RESOLVER SAT
+	print(pycosat.solve(phi))
+
+if __name__ == "__main__":	
+	main(sys.argv[1:])
