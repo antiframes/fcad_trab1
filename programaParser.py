1ca1d713556dfc4517848a8014317d1aa5207250
eliezer <eliezerlribeiro@yahoo.com.br>
2017-02-09 18:36:03 -0200

    exemplo de como dados são guardados

--- programaParser.py
+++ programaParser.py
@@ -5,9 +5,13 @@ def main(argv):
 	c1 = open(argv[0], 'r')
 	net1 = netlist.Netlist(c1)
 	
-	c2 = open(argv[1], 'r')
-	net2 = netlist.Netlist(c2)
-
+	print "Inputs: ", net1.inputs
+	print "Outputs: ",net1.outputs
+	print "Gates: ",net1.gates
+	print "Dicionario: "
+	for n in net1.vetEntSai:
+		print "\t" 
+		print n ,":", net1.vetEntSai[n]
 
 
 if __name__ == "__main__":	
